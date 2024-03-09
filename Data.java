83da3a8bdcbb8eae0dd50ede4ca18c3b92f76305
esparant <116939068+esparant@users.noreply.github.com>
2024-03-09 12:33:46 +0900

    Add files via upload

--- Data.java
+++ Data.java
@@ -1,7 +1,14 @@
-package pack;
+package Memory;
 
 public class Data {
-    public Data() {
-        System.out.println("패키지 pack Data 생성");
+
+    private int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
