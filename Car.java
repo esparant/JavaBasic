53b20144817ea7b3e830d86876f61e99a28e4377
esparant <116939068+esparant@users.noreply.github.com>
2024-03-10 17:33:32 +0900

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,7 +1,8 @@
-package extends1.ex2;
+package poly.car1;
 
-public class Car {
-    public void move() {
-        System.out.println("차를 이동합니다.");
-    }
+public interface Car {
+    void startEngine();
+    void offEngine();
+    void pressAccelerator();
+    String getCarName();
 }
